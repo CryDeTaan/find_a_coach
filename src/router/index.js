e394c06bd1b3bfc8a7d5fe16242c302c5bd77620
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 07:08:59 +0100

    :truck: Added the expected routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
 
 const routes = [
+  { path: '/', redirect: '/coaches' },
+  { path: '/coaches', components: null },
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/coaches/:id',
+    components: null,
+    children: [{ path: 'contact', components: null }],
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
+  { path: '/register', components: null },
+  { path: '/request', components: null },
+  { path: '/:notFound(.*)', components: null },
 ];
 
 const router = createRouter({
