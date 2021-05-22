730f0920bb11369627eddad1870de4364727129b
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 10:45:40 +0100

    🚧 Access dummy state in component.

--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -1,8 +1,17 @@
-<template>Placeholder</template>
+<template>
+  <section>FILTER</section>
+  <section>LIST OF COACHES</section>
+  {{ coaches }}
+</template>
 
 <script>
+import { mapGetters } from 'vuex';
 export default {
   name: 'CoachesList',
+
+  computed: {
+    ...mapGetters('coaches', ['coaches']),
+  },
 };
 </script>
 
