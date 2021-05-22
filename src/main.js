1ac6e1f218bb234cbde0442db73a9e6420402933
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 11:15:10 +0100

    ✨ Base UI Components.

--- src/main.js
+++ src/main.js
@@ -3,4 +3,17 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-createApp(App).use(store).use(router).mount('#app');
+import BaseCard from '@/components/ui/BaseCard';
+import BaseButton from '@/components/ui/BaseButton';
+import BaseBadge from '@/components/ui/BaseBadge';
+
+const app = createApp(App);
+
+app.use(store);
+app.use(router);
+
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
+app.component('base-badge', BaseBadge);
+
+app.mount('#app');
