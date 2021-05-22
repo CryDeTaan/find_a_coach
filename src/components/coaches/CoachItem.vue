ee801ab843165c09e53637aeacbe15359e07e427
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 11:13:13 +0100

    ✨ Coach list and item.

---
+++ src/components/coaches/CoachItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <li>
+    <h3>{{ fullName }}</h3>
+    <h4>${{ coach.rate }}/hour</h4>
+    <div>
+      <span v-for="area in coach.areas" :key="area">{{ area }}</span>
+    </div>
+    <div class="actions">
+      <router-link :to="coachContactLink">Contact</router-link>
+      <router-link :to="coachDetailLink">View Details</router-link>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'CoachItem',
+
+  props: {
+    coach: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  computed: {
+    fullName() {
+      return this.coach.firstName + ' ' + this.coach.lastName;
+    },
+    coachContactLink() {
+      return this.$route.path + '/' + this.coach.id + '/contact'; // coached/c1/contact
+    },
+    coachDetailLink() {
+      return this.$route.path + '/' + this.coach.id; // coached/c1
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  margin: 1rem 0;
+  border: 1px solid #424242;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+h3 {
+  font-size: 1.5rem;
+}
+
+h3,
+h4 {
+  margin: 0.5rem 0;
+}
+
+div {
+  margin: 0.5rem 0;
+}
+
+.actions {
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/views/coaches/CoachesList.vue
+++ src/views/coaches/CoachesList.vue
@@ -1,18 +1,47 @@
 <template>
   <section>FILTER</section>
-  <section>LIST OF COACHES</section>
-  {{ coaches }}
+  <section>
+    <div class="controls">
+      <button>Refresh</button>
+      <router-link to="/register">Register as Coach</router-link>
+    </div>
+    <ul v-if="hasCoaches">
+      <coach-item
+        v-for="coach in coaches"
+        :key="coach.id"
+        :coach="coach"
+      ></coach-item>
+    </ul>
+    <h3 v-else>No Coaches Found.</h3>
+  </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import CoachItem from '@/components/coaches/CoachItem';
+
 export default {
   name: 'CoachesList',
 
+  components: {
+    CoachItem,
+  },
+
   computed: {
-    ...mapGetters('coaches', ['coaches']),
+    ...mapGetters('coaches', ['coaches', 'hasCoaches']),
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
