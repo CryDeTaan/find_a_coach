509057a5e45b46b2f647765b152387f936193037
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 10:15:30 +0100

    ✨ Setup vuex store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import { createStore } from 'vuex';
 
+import coachesModule from './modules/coaches/index';
+import requestsModule from './modules/requests/index';
+
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    coaches: coachesModule,
+    requests: requestsModule,
+  },
 });
