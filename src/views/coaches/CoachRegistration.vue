f2ff789b718bc8032b33a592049797eb3f43617b
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-22 16:53:30 +0100

    ✨ Add a Coach.

--- src/views/coaches/CoachRegistration.vue
+++ src/views/coaches/CoachRegistration.vue
@@ -1,8 +1,27 @@
-<template>Placeholder</template>
+<template>
+  <section>
+    <base-card>
+      <h2>Register as a coach now!</h2>
+      <coach-form @save-data="saveData"></coach-form>
+    </base-card>
+  </section>
+</template>
 
 <script>
+import CoachForm from '@/components/coaches/CoachForm';
 export default {
   name: 'CoachRegistration',
+
+  components: {
+    CoachForm,
+  },
+
+  methods: {
+    saveData(formData) {
+      this.$store.dispatch('coaches/registerCoach', formData);
+      this.$router.replace('/coaches');
+    },
+  },
 };
 </script>
 
