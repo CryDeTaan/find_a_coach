a306f30ffa094795e8b6425317170d97c07dcb42
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-23 06:43:44 +0100

    🗃️ Now storing data in firebase 🔥

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,13 +1,27 @@
 export default {
-  registerCoach(context, data) {
+  async registerCoach(context, data) {
+    const userId = context.rootState.userId;
+
     const coachData = {
-      id: context.rootState.userId,
+      // id: context.rootState.userId, // UserId is stored in URL
       firstName: data.first,
       lastName: data.last,
       description: data.desc,
       hourlyRate: data.rate,
       areas: data.areas,
     };
-    context.commit('registerCoach', coachData);
+    const baseUrl =
+      'https://find-a-coach-using-vue3-default-rtdb.europe-west1.firebasedatabase.app/';
+
+    const response = await fetch(baseUrl + `coaches/${userId}.json`, {
+      method: 'PUT',
+      body: JSON.stringify(coachData),
+    });
+
+    if (!response.ok) {
+      // error ..
+    }
+
+    context.commit('registerCoach', { ...coachData, id: userId });
   },
 };
