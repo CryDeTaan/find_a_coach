fcd543d611f804480664739b89b14abf76a2c5ec
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-25 06:36:19 +0100

    🔒 Added auth guards to routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import store from '@/store/index';
 import CoachesList from '@/views/coaches/CoachesList';
 import CoachDetail from '@/views/coaches/CoachDetail';
 import ContactCoach from '@/views/requests/ContactCoach';
@@ -16,15 +17,33 @@ const routes = [
     component: CoachDetail,
     children: [{ path: 'contact', component: ContactCoach }],
   },
-  { path: '/register', component: CoachRegistration },
-  { path: '/requests', component: RequestsReceived },
-  { path: '/auth', component: UserAuth },
+  {
+    path: '/register',
+    component: CoachRegistration,
+    meta: { requiresAuth: true },
+  },
+  {
+    path: '/requests',
+    component: RequestsReceived,
+    meta: { requiresAuth: true },
+  },
+  { path: '/auth', component: UserAuth, meta: { requiresUnauth: true } },
   { path: '/:notFound(.*)', component: NotFound },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(function (to, from, next) {
+  if (to.meta.requiresAuth && !store.getters['auth/isAuthenticated']) {
+    next('/auth');
+  } else if (to.meta.requiresUnauth && store.getters['auth/isAuthenticated']) {
+    next('/coaches');
+  } else {
+    next();
+  }
 });
 
 export default router;
