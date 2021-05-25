fd4c60da181dcc586e7e74d705ea58d0fa286fd0
CryDeTaan <CryDeTaan@users.noreply.github.com>
2021-05-25 06:48:32 +0100

    ♻️ Refactored auth actions

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,35 +1,30 @@
 import router from '@/router/index';
 export default {
-  async login(context, payload) {
+  login(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'login',
+    });
+  },
+  signup(context, payload) {
+    return context.dispatch('auth', {
+      ...payload,
+      mode: 'signup',
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
     const APIKey = 'AIzaSyA0iUxUnSP8scI4wfoWA2wBW1zZmiPXozg';
-    const authUrl =
+    let url =
       'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' +
       APIKey;
-
-    const response = await fetch(authUrl, {
-      method: 'POST',
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-
-    const responseData = await response.json();
-
-    if (!response.ok) {
-      console.log(responseData);
-      throw new Error(responseData.message || 'Failed to authenticate.');
+    if (mode === 'signup') {
+      url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' +
+        APIKey;
     }
 
-    context.commit('setUser', { responseData });
-  },
-  async signup(context, payload) {
-    const APIKey = 'AIzaSyA0iUxUnSP8scI4wfoWA2wBW1zZmiPXozg';
-    const authUrl =
-      'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + APIKey;
-
-    const response = await fetch(authUrl, {
+    const response = await fetch(url, {
       method: 'POST',
       body: JSON.stringify({
         email: payload.email,
